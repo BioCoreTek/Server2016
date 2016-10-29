698615fc06d09cc1d304fa7b2d1272de1afe95b3
Jeremy Vanseader <madoxlabs@gmail.com>
2016-10-29 15:17:37 -0400

    rejiggered everything. added team name, changed startup, added status

--- spaceServer.js
+++ spaceServer.js
@@ -6,11 +6,11 @@
 //includes...
 var express = require('express');
 var fs = require('fs');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var port = process.env.PORT || 3000;
+var http = require('http');
+var socketIO = require('socket.io');
 
+/////////////
+// GAME LOGGING
 var logpath = ".";
 
 var GameLog = function(name)
@@ -30,12 +30,8 @@ GameLog.prototype.log = function(message)
 	console.log(message);
 }
 
-server.listen(port, function () {
-	console.log('Server listening at port %d', port);
-});
-
-//set up public path (this is super standard for express)
-app.use(express.static(__dirname + '/public'));
+///////////////////
+// CONFIG
 
 // task solutions
 var taskSolutions = {
@@ -45,180 +41,179 @@ var taskSolutions = {
 var stateDelay = {
 	'TaskSchematicsRendering': 5000//180000	// 3 minutes
 }
-
 //these represent different 'pages' that have registered;
 //effectively 'users' are 'pages'
 //this is based on some example chat client out on the interweb...
 // known page ids: 'ipadShields', 'appConsole'
-var numUsers = 0;
-var addedUser = false;
+var teamName = null;
 var gamelog = null;
+var status = "Not playing";
 
-io.on('connection', function (socket) {
-	//create a timer -- maybe transmit a pulse every 30 seconds?
-	setInterval(function(){
-		var now = Date.now();
+//////////////
+// 
+function handleConnect(socket)
+{
+	console.log("connection");
+	setInterval(function () { doStatus(socket);	}, 1000);
+	socket.on('new message', function (data) { handleMessage(socket, data); });
+	socket.on('user register', function (data) {handleUser(socket, data); });
+	socket.on('disconnect', function (data) { handleDisconnect(socket, data); });
+}
 
-		socket.broadcast.emit('timePulse', {
-			timeTick: now
-		});
-	}, 30000);
+function doStatus(socket)
+{
+	var data = {};
+	if (teamName) data.team = teamName;
+	data.status = status;
+	socket.broadcast.emit('status', data);
+}
 
+function handleUser(socket, data)
+{
+	if (teamName) return;
 
-	// when the client emits 'new message', this listens and executes
-	socket.on('new message', function (data) {
+	// we store the username in the socket session for this client
+	teamName = data.username;
 
-		gamelog.log("debug: "+JSON.stringify(data));
-		console.log('new message', data);
+	console.log('user register', teamName);
+	gamelog = new GameLog(teamName);
+	gamelog.log("Ready for Game");
+	status = "Ready to play";
+	io.sockets.emit('new message', { username: teamName, message: { event: 'game', command: 'ready' } });
+}
 
-		if (data.event)
+function handleDisconnect(socket, data)
+{
+  console.log('disconnection');
+}
+
+function handleMessage(socket, data)
+{
+	gamelog.log("debug: "+JSON.stringify(data));
+	console.log('new message', data);
+
+	if (!data.event) return;
+	if (data.event == "game")
+	{
+		if (!data.command) return;
+		else if (data.command == "start") handleGameStart(socket, data);
+		else if (data.command == "stop") handleGameStop(socket, data);
+	}
+	else if (data.event == "task")
+	{
+		if (!data.command) return;
+		else if (data.command == "start") handleTaskStart(socket, data);
+		else if (data.command == "check") handleTaskCheck(socket, data);
+		else if (data.command == "stop") handleTaskStop(socket, data);
+	}
+}
+
+function handleGameStart(socket, data)
+{
+	gamelog.log("Game started");
+	io.sockets.emit('new message', { username: teamName, message: { event: 'timer', command: 'start' } });
+	status = "Playing";
+}
+
+function handleGameStop(socket, data)
+{
+	gamelog.log("Game over");
+	teamName = null;
+	status = "Not Ready"
+	io.sockets.emit('new message', { username: teamName, message: { event: 'game', command: 'stop' } });
+}
+
+function handleTaskStart(socket, data)
+{
+	gamelog.log("Task started");
+}
+
+function handleTaskCheck(socket, data)
+{
+	if (data.data && data.data.result && data.data.taskname)
+	{
+		switch (data.data.taskname)
 		{
-			switch (data.event)
-			{
-				case 'game':
-					if (data.command)
-					{
-						switch (data.command)
-						{
-							case 'start':
-								gamelog.log("Game started");
-								io.sockets.emit('new message', {
-									username: socket.username,
-									message: {
-										event: 'timer',
-										command: 'start'
-									}
-								});
-								break;
-							case 'stop':
-								gamelog.log("Game over");
-								addedUser = false;
-								break;
+			case 'TaskLifesupport':
+				var res = false;
+				if (data.data.result[0] == taskSolutions[data.data.taskname][0] &&
+					data.data.result[1] == taskSolutions[data.data.taskname][1] &&
+					data.data.result[2] == taskSolutions[data.data.taskname][2] &&
+					data.data.result[3] == taskSolutions[data.data.taskname][3])
+				{
+					gamelog.log("TaskLifesupport success");
+					res = true;
+				}
+				else
+				{
+					gamelog.log("TaskLifesupport incorrect");
+				}
+				console.log(data.data.taskname + ' res:', res);
+				// doesn't work
+				//socket.broadcast.emit('new message', {
+				io.sockets.emit('new message', {
+					username: teamName,
+					message: {
+						event: data.event,
+						command: 'result',
+						data: {
+							taskname: data.data.taskname,
+							result: res
 						}
 					}
+				});
+				break;
+		}
+	}
+}
 
-					break;
-				case 'task':
-					if (data.command)
-					{
-						switch (data.command)
-						{
-							case 'start':
-								gamelog.log("Task started");
-								break;
-							case 'check':
-								if (data.data && data.data.result && data.data.taskname)
-								{
-									switch (data.data.taskname)
-									{
-										case 'TaskLifesupport':
-											var res = false;
-											if (data.data.result[0] == taskSolutions[data.data.taskname][0] &&
-												data.data.result[1] == taskSolutions[data.data.taskname][1] &&
-												data.data.result[2] == taskSolutions[data.data.taskname][2] &&
-												data.data.result[3] == taskSolutions[data.data.taskname][3])
-											{
-												gamelog.log("TaskLifesupport success");
-												res = true;
-											}
-											else
-											{
-												gamelog.log("TaskLifesupport incorrect");
-											}
-											console.log(data.data.taskname + ' res:', res);
-											// doesn't work
-											//socket.broadcast.emit('new message', {
-											io.sockets.emit('new message', {
-												username: socket.username,
-												message: {
-													event: data.event,
-													command: 'result',
-													data: {
-														taskname: data.data.taskname,
-														result: res
-													}
-												}
-											});
-											break;
-									}
-								}
-								break;
-							case 'stop':
-								if (data.data && data.data.taskname)
-								{
-									switch (data.data.taskname)
-									{
-										case 'TaskSchematicsRendering':
-											gamelog.log("TaskSchematicsRendering stop");
-											console.log('TaskSchematicsRendering setting Timeout');
-											setTimeout(function() {
-												console.log('Timeout done TaskSchematicsRendering');
-												gamelog.log("TaskLifesupport set");
-												io.sockets.emit('new message', {
-													username: socket.username,
-													message: {
-														event: 'state',
-														command: 'set',
-														data: {
-															group: 'lifesupport',
-															state: 'failure'
-														}
-													}
-												});
-												io.sockets.emit('new message', {
-													username: socket.username,
-													message: {
-														event: 'state',
-														command: 'interrupt',
-														data: {
-															group: 'lifesupport'
-														}
-													}
-												});
-											}, stateDelay.TaskSchematicsRendering);
-											break;
-									}
-								}
-								break;
+function handleTaskStop(socket, data)
+{
+	if (data.data && data.data.taskname)
+	{
+		switch (data.data.taskname)
+		{
+			case 'TaskSchematicsRendering':
+				gamelog.log("TaskSchematicsRendering stop");
+				console.log('TaskSchematicsRendering setting Timeout');
+				setTimeout(function() {
+					console.log('Timeout done TaskSchematicsRendering');
+					gamelog.log("TaskLifesupport set");
+					io.sockets.emit('new message', {
+						username: teamName,
+						message: {
+							event: 'state',
+							command: 'set',
+							data: {
+								group: 'lifesupport',
+								state: 'failure'
+							}
 						}
-					}
-					break;
-			}
+					});
+					io.sockets.emit('new message', {
+						username: teamName,
+						message: {
+							event: 'state',
+							command: 'interrupt',
+							data: {
+								group: 'lifesupport'
+							}
+						}
+					});
+				}, stateDelay.TaskSchematicsRendering);
+				break;
 		}
-	});
+	}
+}
 
-	// when the client emits 'user register', this listens and executes
-	socket.on('user register', function (data) {
-		if (addedUser) return;
+//////////////
+// MAIN
+var app = express();
+app.use(express.static(__dirname + '/public'));
 
-		// we store the username in the socket session for this client
-		socket.username = data.username;
-		++numUsers;
-		addedUser = true;
+var port = process.env.PORT || 3000;
+var server = http.createServer(app);
+server.listen(port, function () { console.log('Server listening at port %d', port); });
 
-		console.log('user register', socket.username);
-		gamelog = new GameLog(socket.username);
-		gamelog.log("Ready for Game");
-
-		// echo globally (all clients) that a person has connected
-		socket.broadcast.emit('user joined', {
-			username: socket.username,
-			numUsers: numUsers
-		});
-	});
-
-	// when the user disconnects, remove them and broadcast that they have left...
-	socket.on('disconnect', function () {
-		if (addedUser) {
-			--numUsers;
-
-			console.log('disconnect', socket.username);
-
-			// echo globally that this client has left
-			socket.broadcast.emit('user left', {
-				username: socket.username,
-				numUsers: numUsers
-			});
-		}
-	});
-});
+var io = socketIO(server);
+io.on('connection', handleConnect);
